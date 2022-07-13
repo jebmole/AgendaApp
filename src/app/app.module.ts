3c7d0ffb5112c1ae5be9afadce54f5d2f3d5e6ec
Alejandro Betancur <alejazzoo8@gmail.com>
2022-07-12 20:16:10 -0500

    Se implementan custom modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HeaderComponent} from './components/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SharedModule } from './components/shared/shared.module';
 import { TodoComponent } from './components/todo/todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    SidebarComponent,
     TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
