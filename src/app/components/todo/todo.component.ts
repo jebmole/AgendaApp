fe4580b09ea13d01c3e4e0eab3e7d17ef1797f27
Alejandro Betancur <alejazzoo8@gmail.com>
2022-07-12 21:07:29 -0500

    Se implementar NgClass

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,25 +1,50 @@
+import { Tarea } from './models/tarea';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent implements OnInit {
-  tareasPendientes: string[] = [];
+  tareasPendientes: Tarea[] = [];
   mostrarInfo: boolean = false;
-  constructor() { }
+  colorUrgente: string = 'text-danger';
+  colorNoUrgente: string = 'text-info';
+  colorMedio: string = 'text-warning';
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  agregarPendiente(input: HTMLInputElement){
-    this.tareasPendientes.push(input.value);
+  agregarPendiente(
+    inputPendiente: HTMLInputElement,
+    inputDias: HTMLInputElement
+  ) {
+    const tarea: Tarea = {
+      descripcion: inputPendiente.value,
+      dias: parseInt(inputDias.value),
+    };
+
+    this.tareasPendientes.push(tarea);
     this.mostrarInfo = true;
-    input.value = "";
+    inputPendiente.value = '';
+    inputDias.value = '';
   }
 
-  eliminar(index: number){
-    this.tareasPendientes.splice(index,1);
+  eliminar(index: number) {
+    this.tareasPendientes.splice(index, 1);
+  }
+
+  getColor(dias: number): string {
+    let color = '';
+    if (dias >= 4) {
+      color = this.colorNoUrgente;
+    } else if (dias < 4 && dias >= 1) {
+      color = this.colorMedio;
+    } else {
+      color = this.colorUrgente;
+    }
+
+    return color;
   }
 }
